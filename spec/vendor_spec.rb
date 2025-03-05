dbb2681b4df24a56de584e67a0b103bf261356d9
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-05 10:39:50 -0600

    tests for the check_stock method

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -5,6 +5,8 @@ require './lib/vendor'
 RSpec.describe Vendor do
   before :each do
     @vendor =  Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   it "exists" do
@@ -14,5 +16,14 @@ RSpec.describe Vendor do
     expect(@vendor.name).to eq("Rocky Mountain Fresh")
     expect(@vendor.inventory).to eq({})
   end
-  
+
+  describe '#check_stock' do
+   it "checks the stock" do
+
+    @vendor.check_stock(@item1)
+
+    expect(@vendor.check_stock(@item1)).to eq(0)
+
+   end
+  end
 end
