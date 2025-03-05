d4a56ca02010658c94338113d72382dcef08054a
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-05 11:05:30 -0600

    created tests for the stock method

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -19,11 +19,26 @@ RSpec.describe Vendor do
 
   describe '#check_stock' do
    it "checks the stock" do
+      @vendor.check_stock(@item1)
 
-    @vendor.check_stock(@item1)
-
-    expect(@vendor.check_stock(@item1)).to eq(0)
-
+      expect(@vendor.check_stock(@item1)).to eq(0)
    end
+  end
+  describe '#stock' do
+    it 'adds to the stock' do
+      @vendor.stock(@item1, 30)
+
+      expect(@vendor.inventory).to eq ({@item1 => 30})
+      expect(@vendor.check_stock(@item1)).to eq(30)
+
+      @vendor.stock(@item1, 25)
+      #expect(@vendor.inventory).to eq ({@item1 => 55})
+      expect(@vendor.check_stock(@item1)).to eq(55)
+
+      @vendor.stock(@item2, 12)
+      expect(@vendor.check_stock(@item2)).to eq(12)
+      expect(@vendor.inventory).to eq ({@item1 => 55, @item2 => 12})
+
+    end
   end
 end
