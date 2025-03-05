2c6337c0ba44fec9c1fde1f5c12443303e01c6e4
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-05 12:01:10 -0600

    tests the vendors_that_sell method

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'pry'
 require './lib/item'
 require './lib/vendor'
 require './lib/market'
@@ -8,6 +9,7 @@ RSpec.describe Market do
     @market = Market.new("South Pearl Street Farmers Market")
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
     @vendor2 = Vendor.new("Ba-Nom-a-Nom") 
+    @vendor3 = Vendor.new("Palisade Peach Shack") 
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: "$0.50"})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
@@ -34,4 +36,30 @@ RSpec.describe Market do
      expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
     end
   end
+  describe '@vendor_names' do
+    it "returns the vendor names" do
+
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+  end
+
+  describe '#vendors_that_sell' do
+    it "returns the vendors that sell a particular item" do
+      @vendor1.stock(@item1, 35)
+      @vendor1.stock(@item2, 7)
+      @vendor2.stock(@item3, 50)
+      @vendor3.stock(@item1, 65)
+
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+
+      expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
+      expect(@market.vendors_that_sell(@item3)).to eq([@vendor2])
+    end
+  end
+ 
 end
